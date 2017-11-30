63fafc2b6c205047146b5815750dd00462fd3465
Jesse Gerard <jessehgerard@gmail.com>
2017-11-30 14:00:23 -0500

    for refrence

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 let inquirer = require('inquirer');
 var mysql = require("mysql");
-var baeeFunc = require("baeeFunk");
+var baeeFunc = require("./baeeFunk.js");
+var clear = require('clear');
+
+clear();
+console.log("\nWelcome to LazyBay!!!\n==================================================\nCommand Options:\n==================================================\n1)'POST an auction item'\nThis will create a new auction item for folks to bid on. Set any starting price ya like!\n\n2)'BID on a currently listed item.\nThis will show you all items available.\n\n3)'movie-this' <input movie name here>\nThis will give you lots of info for a movie you select. \n\n4)'do-what-it-says'\nThis will randomly call a LIRI command for you.\n==================================================\n" )
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -15,24 +19,49 @@ connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
 
-const queryBids = () => {
 
+inquirer.prompt([
+    {
+      type: "list",
+      name: "choice",
+      message: "POST an item or BID an item:",
+      choices: ["POST", "BID"]
+    }
+]).then(function(user){
+  console.log("You entered the integer: " user.choice);//user.inputNumb);
+  var finalResult = queryAction(user.choice); //user.inputNumb);
+});
+
+const incPost = () => {
   inquirer.prompt([
     {
-      type: "input",
-      name: "choice",
-      message: "POST an item or BID an Item:"
+      type: 'list',
+      name: 'post',
+      message: 'What item would you like to post?',
+
     }
-  ]).then(function(user){
-    console.log("You entered the integer: " user.choice);//user.inputNumb);
-    var finalResult = queryAction(user.choice); //user.inputNumb);
+  ]).then(()=>{
+
   });
 };
 
 
-  queryAllSongs();
-  queryPopSongs();
-});
+
+
+function queryAction(choice){
+  if (choice === BID){
+
+  }else {
+    //do post action
+  }
+}
+
+const queryBids = () => {
+
+};
+
+
+
 function queryAllSongs() {
   connection.query("SELECT * FROM songs", function(err, res) {
     for (var i = 0; i < res.length; i++) {
